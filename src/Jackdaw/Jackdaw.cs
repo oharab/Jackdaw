289a96b7f033d2a32b3e94fd101e9c8800229cfd
Ben O'Hara <bpo@robotparade.co.uk>
2012-09-13 23:39:28 +0100

    Added basic Jackdaw object & wraps ready for NTS

---
+++ src/Jackdaw/Jackdaw.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * User: ben
+ * Date: 13/09/2012
+ * Time: 23:22
+ * 
+ */
+using System;
+
+namespace Jackdaw
+{
+	/// <summary>
+	/// Description of Jackdaw.
+	/// </summary>
+	public class Jackdaw
+	{
+		public string ShapeFile { get; set; }
+		public int Boundaries { get; private set; }
+	}
+}
