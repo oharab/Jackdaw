b97b5b4fda86a457341089f6bbc4f6029f37abdf
Ben O'Hara <bpo@robotparade.co.uk>
2012-09-17 22:01:22 +0100

    Reads headers from top left region of first worksheet.

--- src/Jackdaw/FileParsers/XLSFileParser.cs
+++ src/Jackdaw/FileParsers/XLSFileParser.cs
@@ -5,6 +5,12 @@
  * 
  */
 using System;
+using System.Collections.Generic;
+using System.IO;
+
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS.FileSystem;
+using NPOI.SS.UserModel;
 
 namespace Jackdaw.FileParsers
 {
@@ -13,15 +19,42 @@ namespace Jackdaw.FileParsers
 	/// </summary>
 	public class XLSFileParser:IFileParser
 	{
+		private readonly string fileName;
+		private readonly HSSFWorkbook workbook;
+		
+		private ISheet worksheet;
 		
 		public XLSFileParser(string fileName)
 		{
+			this.fileName=fileName;
+			using (FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+			{
+				this.workbook = new HSSFWorkbook(file);
+			}
 		}
 		
+		private string[] headers;
 		public string[] Headers {
 			get {
-				throw new NotImplementedException();
+				initializeWorkbook();
+				return headers;
 			}
+		}
+		private bool isinitialized;
+		private void initializeWorkbook()
+		{
+			if(isinitialized)
+				return;
+			this.worksheet=this.workbook.GetSheetAt(0);
+			var headerRow=this.worksheet.GetRow(
+				this.worksheet.FirstRowNum
+			);
+			List<string> headerlist=new List<string>();
+			foreach (var cell in headerRow.Cells) {
+				headerlist.Add(cell.ToString());
+			}
+			headers=headerlist.ToArray();
+			isinitialized=true;
 		}
 	}
 }
